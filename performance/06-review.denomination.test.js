f6b7656b7e38ff19310f5d352439ddf77ac24c20
Geissy Araújo <geissyaraujo@gmail.com>
2025-11-04 23:43:42 -0300

    Realizei Ajuste no teste review.denomination

--- performance/06-review.denomination.test.js
+++ performance/06-review.denomination.test.js
@@ -2,7 +2,8 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-    iterations:1,
+    vus: 10,
+    duration: "30s",
 
     thresholds: {
         http_req_failed: ["rate<0.01"],
